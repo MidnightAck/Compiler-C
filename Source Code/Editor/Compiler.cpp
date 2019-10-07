7f6dbc89991c0e52532ca63e8b71af568e22f424
CrowFea <wendaoliu40@outlook.com>
2019-10-07 12:40:29 +0800

    源代码

---
+++ Source Code/Editor/Compiler.cpp
@@ -0,0 +1,10 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QDebug>
+#include<QFileDialog>
+#include <QFile>
+#include <QTextStream>
+using namespace std;
+
+void run(){
+ }
---
+++ Source Code/Lexical_Analyze/test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+    int a,c;
+    float b;
+    char d,res=1;
+
+    if(d=='1'){
+         res=a+(b*c)/4;
+    }
+    else{
+        while(res<100){
+            res+=1;
+        }
+    }
+    return;
+}
